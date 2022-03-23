5a1b99ae5ed65e813d1206a0b510b895ddcc85a7
deniseferreira06 <83511358+deniseferreira06@users.noreply.github.com>
2022-03-23 20:25:25 -0300

    Add files via upload

---
+++ deniseferreira06/todolistcli-dam202102/src/components/TaskCard/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TouchableOpacity, Text, TouchableOpacityProps } from 'react-native';
+
+import { styles } from './styles';
+
+interface TaskCardProps extends TouchableOpacityProps {
+    title: string;
+    onPress: ()=>void;
+}
+
+export function TaskCard({ title, onPress }: TaskCardProps) {
+    return (
+        <TouchableOpacity 
+          style={styles.buttonTask}
+          onPress={onPress}
+          >
+          <Text style={styles.textTask}> {title} 
+          </Text>
+        </TouchableOpacity>
+    );
+}
